773152aa1da5e6ecf7243ed8fc1c23fbd6b1b11b
sathish447 <sathishprabu447@gmail.com>
2026-06-12 23:49:28 +0530

    dashboard page changed admin lte format

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,33 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('adminlte::page')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+
+<div class="row">
+
+    <div class="col-lg-3">
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>0</h3>
+                <p>Family Members</p>
             </div>
         </div>
     </div>
-</x-app-layout>
+
+    <div class="col-lg-3">
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>₹0</h3>
+                <p>Total Assets</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+@stop
